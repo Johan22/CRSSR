86490bfcd7000eafd5a38140f09061b23c7ff835
Johan22 <joh.julien@gmail.com>
2019-01-25 17:35:00 -0500

    [FEAT] Add second rpc for filters Add a button to load new results

--- components/Filters.js
+++ components/Filters.js
@@ -1,38 +1,49 @@
 
-const Filters = (props) => (
-  <div className="filters">
-    {props.filters.map((filter) => (
-      <div className="filter-container">
-        <div>{filter.name}</div>
+// const Filters = (props) => (
 
-        {filter.options && filter.options.length > 0 && filter.options.map((option) => (
-          <div>{option && option.label}</div>
+class Filters extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('filters');
+    console.log(props);
+  }
+
+  render() {
+    return (
+      <div className="filters">
+
+        {this.props.filters.map((filter) => (
+          <div className="filter-container">
+            <div>{filter.name}</div>
+
+            {filter.options && filter.options.length > 0 && filter.options.map((option) => (
+              <div>{option && option.label}</div>
+            ))}
+          </div>
+
         ))}
+
+        <style jsx>{`
+          .filters {
+            width: 260px;
+            padding-bottom: 20px;
+            position: fixed;
+            top: 75px;
+            left: 0;
+            bottom: 0;
+            background-color: #f0f0f0;
+            overflow-y: scroll;
+            z-index: 100001;
+            font-size: 16px;
+
+            .filter-container {
+              padding: 20px;
+            }
+          }
+        `}</style>
       </div>
-
-    ))}
-
-    <style jsx>{`
-      .filters {
-        width: 260px;
-        padding-bottom: 20px;
-        position: fixed;
-        top: 75px;
-        left: 0;
-        bottom: 0;
-        background-color: #f0f0f0;
-        overflow-y: scroll;
-        z-index: 100001;
-        font-size: 16px;
-
-        .filter-container {
-          padding: 20px;
-        }
-      }
-    `}</style>
-  </div>
-
-
-)
+    );
+  }
+}
 
 export default Filters
